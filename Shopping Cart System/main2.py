e2dea63db61e9fe3c7e1bf495289d9ce332f1639
ranmahalal <ran.mahalal@gmail.com>
2025-08-12 17:43:11 +0300

    removed main2

--- Shopping Cart System/main2.py
+++
@@ -1,9 +0,0 @@
-from shopping_cart import ShoppingCart
-from store import Store
-from item import Item
-
-def main():
-    store = Store('items.yml)')
-    print("store items are:")
-    for item in store.get_items():
-        print(item)
